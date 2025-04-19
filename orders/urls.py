50b59d374ad299972670526bc30c3f609806b107
Angelvaldes117 <angelluisvaldes110@gmail.com>
2025-04-19 10:08:20 -0500

    Videos de la creacion de una pagina web de una cafeteria

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from  .views import MyOrderView, CreateOrderProductView
-
+from .views import MyOrderView, CreateOrderProductView, DeleteMyOrderView
 
 urlpatterns = [
-    path('mi-orden', MyOrderView.as_view(), name="my_order" ),
-    path('agregar-producto', CreateOrderProductView.as_view(), name="add_product")
+    path('mi-orden', MyOrderView.as_view(), name="my_order"),
+    path('agregar-producto', CreateOrderProductView.as_view(), name="add_product"),
+    path('delete-my-order/', DeleteMyOrderView.as_view(), name='delete_my_order'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,20 +1,36 @@
 from django.views.generic import DetailView, CreateView
+from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 
 from .models import Order
 from .forms import OrderProductForm
+
 
 class MyOrderView(LoginRequiredMixin, DetailView):
     model = Order
     template_name = "orders/my_order.html"
     context_object_name = "order"
 
-    def get_object(self, queryset = None):
+    def get_object(self, queryset=None):
         return Order.objects.filter(is_active=True, user=self.request.user).first()
-    
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        order = self.get_object()
+
+        if order:
+            total = sum([po.product.price for po in order.orderproduct_set.all()])
+        else:
+            total = 0
+
+        context["total"] = total
+        return context
+
+
 class CreateOrderProductView(LoginRequiredMixin, CreateView):
-    template_name= "orders/create_order_product.html"
+    template_name = "orders/create_order_product.html"
     form_class = OrderProductForm
     success_url = reverse_lazy('my_order')
 
@@ -27,3 +43,11 @@ class CreateOrderProductView(LoginRequiredMixin, CreateView):
         form.instance.quantity = 1
         form.save()
         return super().form_valid(form)
+
+
+class DeleteMyOrderView(LoginRequiredMixin, View):
+    def post(self, request, *args, **kwargs):
+        order = Order.objects.filter(is_active=True, user=request.user).first()
+        if order:
+            order.delete()
+        return redirect('my_order')
